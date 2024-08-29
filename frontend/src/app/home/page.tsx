7e7669b8359f8a3321b6651117095822ea304235
Valderrama20 <jose23122009@gmai.com>
2024-08-29 14:25:13 +0200

    Se agrego el .env

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -8,6 +8,7 @@ export default function () {
   const { user, setUser } = userStore((user) => user);
   const { response, fetchData } = useFetchData(getUserById);
 
+  console.log(process.env.NEXT_PUBLIC_URL_BASE);
   const getUser = async () => {
     await fetchData({ url: "1" });
   };
@@ -19,9 +20,11 @@ export default function () {
   return (
     <div>
       <h1>Informacion de la api</h1>
-      <pre>{JSON.stringify(response, null, 2)}</pre>
+      <pre>{JSON.stringify(response, null, 2) || "sin informacion"}</pre>
+
       <h1>Mi estado local:</h1>
-      <pre>{JSON.stringify(user, null, 2)}</pre>
+      <pre>{JSON.stringify(user, null, 2) || "sin informacion"}</pre>
+
       <div className=" space-x-5">
         <button onClick={getUser}>Hacer llamada a la api</button>
         <button onClick={modifyUser}>
