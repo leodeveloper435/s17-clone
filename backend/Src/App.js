65c9f479dabbc28d750396188cf8be137ef72e57
stimado9021 <quinteroenrrique321@gmail.com>
2024-09-02 10:24:56 -0500

    primer deploy

--- backend/Src/App.js
+++ backend/Src/App.js
@@ -1,6 +1,7 @@
 import express from "express"
 import Session from "express-session"
 import dotenv from "dotenv"
+import morgan from "morgan";
 import AuthRouter from "./Routes/Auth/Auth.routes.js"
 import { __dirname } from "./Utils/RouteAbsolute.util.js"
 //Config to Express
@@ -12,7 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 app.use(express.static(__dirname + "/public"));
 dotenv.config()
-
+app.use(morgan("dev"));
 //Cookies
 app.use(Session({
     secret : process.env.SESSION_SECRET,
--- backend/Src/Config/DB.config.js
+++ backend/Src/Config/DB.config.js
@@ -3,13 +3,18 @@ import dotenv from 'dotenv';
 import User from '../DTO/Models/Users/User.model.js';
 
 dotenv.config();
-
+console.log(process.env.DB_HOST)
 export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST || 'localhost',
   dialect: 'postgres',
   port: process.env.DB_PORT || 5432,
-  logging: false, 
-  dialect : "postgres"
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: true // Usa esto solo en desarrollo
+    }
+  },
+  logging: console.log 
 });
 
 
