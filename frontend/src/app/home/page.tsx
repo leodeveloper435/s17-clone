3f25cc2a24466cd31fdc021cf4d11411a6653e5f
Lalomax <lalomax365@gmail.com>
2024-09-05 16:31:11 -0400

    fix: removed response state

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -6,29 +6,29 @@ import { getUserById } from "@/services/userServices";
 
 export default function Page() {
   const { user, setUser } = userStore((user) => user);
-  const { response, fetchData } = useFetchData(getUserById);
+  const { fetchData } = useFetchData(getUserById);
 
   const getUser = async () => {
     await fetchData({ url: "1" });
   };
 
-  const modifyUser = async () => {
-    response ? setUser(response) : alert("tienes que llamar a la api primero");
-  };
+  // const modifyUser = async () => {
+  //   response ? setUser(response) : alert("tienes que llamar a la api primero");
+  // };
 
   return (
     <div>
       <h1>Informacion de la api</h1>
-      <pre>{JSON.stringify(response, null, 2) || "sin informacion"}</pre>
+      {/* <pre>{JSON.stringify(response, null, 2) || "sin informacion"}</pre> */}
 
       <h1>Mi estado local:</h1>
       <pre>{JSON.stringify(user, null, 2) || "sin informacion"}</pre>
 
       <div className=" space-x-5">
         <button onClick={getUser}>Hacer llamada a la api</button>
-        <button onClick={modifyUser}>
+        {/* <button onClick={modifyUser}>
           Actualizar infomacion en estado local
-        </button>
+        </button> */}
       </div>
     </div>
   );
