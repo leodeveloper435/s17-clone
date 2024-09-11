068fff70ae1c1cbcf2b56eec3aa6124334d95aef
Valderrama20 <jose23122009@gmai.com>
2024-09-11 16:54:38 +0200

    Se corrigio nuevamente el auth

--- frontend/src/app/auth/withAuth.tsx
+++ frontend/src/app/auth/withAuth.tsx
@@ -7,18 +7,22 @@ interface TypeProps {
   children?: React.ReactNode;
 }
 
-const withAuth =
-  (WrappedComponent: React.ComponentType<any>) => (props: any) => {
+const withAuth = (WrappedComponent: React.ComponentType<any>) => {
+  const Wrapper = (props: any) => {
     const router = useRouter();
     const [isLogin, setIsLogin] = useState(false);
 
     useEffect(() => {
-      const getToken = JSON.parse(localStorage.getItem("userStore") || "{}");
-
-      getToken?.state?.user.token ? setIsLogin(true) : router.push("/login");
+      if (typeof window !== "undefined") {
+        const getToken = JSON.parse(localStorage.getItem("userStore") || "{}");
+        getToken?.state?.user.token ? setIsLogin(true) : router.push("/login");
+      }
     }, [router]);
 
     return isLogin ? <WrappedComponent {...props} /> : <h1>Cargando</h1>;
   };
+
+  return Wrapper;
+};
 
 export default withAuth;
