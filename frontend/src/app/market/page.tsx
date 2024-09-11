c53ae283401f3bef10bfb30ad9673351dc0b8220
Valderrama20 <jose23122009@gmai.com>
2024-09-11 11:28:45 +0200

    se agrego el header a market

--- frontend/src/app/market/page.tsx
+++ frontend/src/app/market/page.tsx
@@ -8,6 +8,7 @@ import { toast } from "sonner";
 import CropPrices from "../../components/market/CropPrices";
 import CurrencyExchange from "../../components/market/CurrencyExchange";
 import AgriculturalInputs from "../../components/market/AgriculturalInputs";
+import Header from "../common/Header";
 
 interface CropPricesProps {
   marketData: string; // Replace 'any' with the actual type of marketData
@@ -28,23 +29,26 @@ const Market = () => {
   }, []);
 
   return (
-    <div className="min-h-screen bg-orange-50 py-8">
-      <Head>
-        <title>Agricultural Data</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <Header />
+      <div className="min-h-screen bg-orange-50 py-8">
+        <Head>
+          <title>Agricultural Data</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className="container mx-auto px-4 text-black">
-        <h1 className="text-3xl font-bold mb-8 text-center ">
-          Datos actuales de mercado
-        </h1>
-        <div className="space-y-8 ">
-          <CropPrices marketData={marketData} />
-          <CurrencyExchange />
-          <AgriculturalInputs />
-        </div>
-      </main>
-    </div>
+        <main className="container mx-auto px-4 text-black">
+          <h1 className="text-3xl font-bold mb-8 text-center ">
+            Datos actuales de mercado
+          </h1>
+          <div className="space-y-8 ">
+            <CropPrices marketData={marketData} />
+            <CurrencyExchange />
+            <AgriculturalInputs />
+          </div>
+        </main>
+      </div>
+    </>
   );
 };
 
