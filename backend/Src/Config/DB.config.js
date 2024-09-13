cb6581400d57362f7fef3dd3e197e1eebf684b0e
leoyasu <leandroyasutake@gmail.com>
2024-09-13 17:51:07 -0300

    añadido validador de esquema para post de new campo

--- backend/Src/Config/DB.config.js
+++ backend/Src/Config/DB.config.js
@@ -12,12 +12,12 @@ export const sequelize = new Sequelize(
     dialect: "postgres",
     port: process.env.DB_PORT || 5432,
     logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
+    // dialectOptions: {
+    //   ssl: {
+    //     require: true,
+    //     rejectUnauthorized: false,
+    //   },
+    // },
   }
 );
 
