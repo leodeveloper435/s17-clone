bdef5e19896635dca64213a3b057133ec9e286c7
leoyasu <leandroyasutake@gmail.com>
2024-09-17 17:10:36 -0300

    jwt 7

--- backend/Src/App.js
+++ backend/Src/App.js
@@ -72,7 +72,7 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: { maxAge: 1000 * 60 * 24 * 7 },
+    cookie: { maxAge: 1000 * 60 * 24 * 7, sameSite: "None" },
   })
 );
 
