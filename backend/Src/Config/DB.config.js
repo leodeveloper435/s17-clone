e1b70f1a7414426ff0f39122947e3ec3f7d1f0a3
leoyasu <leandroyasutake@gmail.com>
2024-09-17 15:28:19 -0300

    copia de repo, test de deploy en railway fix env uri

--- backend/Src/Config/DB.config.js
+++ backend/Src/Config/DB.config.js
@@ -3,23 +3,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST || "localhost",
-    dialect: "postgres",
-    port: process.env.DB_PORT || 5432,
-    logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+export const sequelize = new Sequelize(process.env.DB_URI, {
+  dialect: "postgres", 
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true, 
+      rejectUnauthorized: false,
     },
-  }
-);
+  },
+});
 
 sequelize
   .authenticate()
