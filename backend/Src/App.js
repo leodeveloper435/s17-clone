760e6e6c0262c49cf537a5d5ef5e849b1ddec5ce
Valderrama20 <jose23122009@gmai.com>
2024-09-17 09:28:42 +0200

    Se suluciono el problema de las credenciales

--- backend/Src/App.js
+++ backend/Src/App.js
@@ -38,7 +38,8 @@ const swaggerSpec = {
 const swagger = swaggerjsdoc(swaggerSpec);
 
 //Middlewares Global
-app.use(cors());
+app.use(cors({origin: "http://localhost:3000",credentials:true}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
